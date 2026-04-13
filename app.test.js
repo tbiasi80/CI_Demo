6307d8b82afcda01976814b49d03879b3a45b923
Thomas Biasi <thomas_biasi@libero.it>
2026-04-13 19:28:19 +0200

    Test prettier

--- app.test.js
+++ app.test.js
@@ -1,13 +1,11 @@
-const request = require('supertest');
-const { describe, it, expect } = require('@jest/globals');
-const app = require('./app');
+const request = require("supertest");
+const { describe, it, expect } = require("@jest/globals");
+const app = require("./app");
 
-describe('GET /', () => {
+describe("GET /", () => {
   it(`should return 'Works on my machine.'`, async () => {
-    const res = await request(app)
-      .get('/')
-      .expect(200);
+    const res = await request(app).get("/").expect(200);
 
-    expect(res.text).toBe('Works on my machine.');
+    expect(res.text).toBe("Works on my machine.");
   });
 });
