664b5dfdd1afb5b0c7b0ba80dcca9023b1f9f275
Thomas Biasi <thomas_biasi@libero.it>
2026-04-18 12:00:35 +0200

    Modifiche per introduzione dockerhub

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 
 const port = 3000;
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   //Sintassi per dire a ESLint di ignorare il log che segue il commnento.
   // eslint-disable-next-line no-console
   console.log(`Server listening on port ${port}`);
